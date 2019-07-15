f6aa9253243b28e93c24c49f4a2cfa5fd008610c
Lachlan Wilson <s3722123@student.rmit.edu.au>
2019-07-15 16:36:20 +1000

    Initial Commit

---
+++ RepoDemo/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  RepoDemo
+//
+//  Created by Lachlan  Wilson on 15/7/19.
+//  Copyright © 2019 Lachlan  Wilson. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
